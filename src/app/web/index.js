e8822f63381c1791a816ca2a1e4d825bdb0f2c52
Floyd Price <f.price@floydprice.com>
2019-03-11 21:58:13 +0000

    remove login.dfe.healthchack and replace with express-status-monitor

--- src/app/web/index.js
+++ src/app/web/index.js
@@ -1,14 +1,14 @@
-const healthCheck = require('login.dfe.healthcheck');
+const statusMonitor = require('express-status-monitor')();
 
 const example = require('./example');
 
 
 const mountRoutes = (app, config) => {
-  app.use('/healthcheck', healthCheck({
-    config,
-  }));
+  app.use(statusMonitor);
 
-  app.use('/', example.home);
+  app.get('/status', statusMonitor.pageRoute)
+
+  app.get('/', example.home);
 };
 
 
